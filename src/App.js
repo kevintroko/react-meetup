886c8b086845f1162e415cc3c1136fa785114ced
Kevin Cabrera <kevin.cabrera@consultant.volvo.com>
2022-09-19 19:18:20 +0200

    Import css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Backdrop from './components/Backdrop';
+import Modal from './components/Modal';
 import Todo from './components/Todo';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
       <Todo title="Learn React"/>
       <Todo title="Master React"/>
       <Todo title="Create own site"/>
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,17 @@
-import './Todo.css';
-
 const Todo = ({ title }) => {
+    
+    const deleteHandler = (e) => {
+        console.log(e);
+    };
+
     return <>
         <div className='card'>
-            <h2>{ title }</h2>
+            <h2>{title}</h2>
             <div className='actions'>
-                <button>Delete</button>
+                <button onClick={ deleteHandler }
+                    className="btn">
+                    Delete
+                </button>
             </div>
         </div>
     </>;
