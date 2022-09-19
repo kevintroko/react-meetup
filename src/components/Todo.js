879342f04ee5596346a3d64cee69253d8039ff89
Kevin Cabrera <kevin.cabrera@consultant.volvo.com>
2022-09-19 14:38:22 +0200

    Add ToDo component

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+
+const Todo = () => {
+    return <>
+        <div className='card'>
+            <h2>Title</h2>
+            <div className='actions'>
+                <button>Delete</button>
+            </div>
+        </div>
+    </>;
+}
+
+export default Todo;
