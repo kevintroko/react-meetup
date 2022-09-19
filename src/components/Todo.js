c01d40f2fd3f275871a4a52471a08a5c165f62b6
Kevin Cabrera <kevin.cabrera@consultant.volvo.com>
2022-09-19 20:49:58 +0200

    Move logic to ToDo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,18 +1,28 @@
+import { useState } from 'react';
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
 const Todo = ({ title }) => {
-    
+    const [open, setOpen] = useState(false);
+    // useStateFalse returns an array with two elements
+
     const deleteHandler = (e) => {
-        console.log(e);
+        console.log(e, open);
+        setOpen(true);
     };
 
     return <>
         <div className='card'>
             <h2>{title}</h2>
             <div className='actions'>
-                <button onClick={ deleteHandler }
+                <button onClick={deleteHandler}
                     className="btn">
                     Delete
                 </button>
             </div>
+
+            { open && <Modal /> }
+            { open && <Backdrop /> }
         </div>
     </>;
 }
